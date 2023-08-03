0f27fa5bb28f6a04760d4f3f7a30f88d77d5099c
Berke Ozten <berkeozten@gmail.com>
2023-08-03 16:54:55 -0700

    Aug3-check

--- Server/models/bookings.js
+++ Server/models/bookings.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 const bookingSchema = new mongoose.Schema({
     date:{
-        type: Date
+        type: Date // Year - Month - Day only
     },
     level: {
         type: String
         // value in beginner, intermediate, advance
     },
+    status: String, // 1- Open 2- Full 3- Canceled
     students: [
         {
-            username: String,            
+            username: String,
+            status: Number // 1- Ok 2- Waitlist 3- Rejected            
         }
     ]
     
