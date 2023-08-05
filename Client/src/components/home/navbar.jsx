6af1d0560206fbb577a60b986e7da82e691fa544
Berke Ozten <berkeozten@gmail.com>
2023-08-05 14:27:18 -0700

    navbar update tried

--- Client/src/components/home/navbar.jsx
+++ Client/src/components/home/navbar.jsx
@@ -3,68 +3,83 @@ import { Link } from 'react-scroll'
 import '../../css/navbar.css'
 import Logo from '../../images/logo';
 import { useLocation } from 'react-router-dom'
-import {Squash as Hamburger} from 'hamburger-react';
+import { Squash as Hamburger } from 'hamburger-react';
 import user from '../../images/user.svg'
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 var style_nav;
 
 function Navbar() {
-    const [show, setShow] = useState(true);
-    const [isOpen, setOpen] = useState(false);
-    
-    if(isOpen === false){
-      style_nav ="hide-nav";
-    }else{
-      style_nav ="show-nav";
+  const [show, setShow] = useState(true);
+  const [isOpen, setOpen] = useState(false);
+
+  if (isOpen === false) {
+    style_nav = "hide-nav";
+  } else {
+    style_nav = "show-nav";
+  }
+
+  const setClose = () => {
+    setOpen(false);
+  }
+
+  const controlNavbar = () => {
+    if (window.scrollY > 700) {
+      setShow(false);
+    } else {
+      setShow(true);
     }
-  
-    const setClose = () => {
-      setOpen(false);
-    }
-    
-    const controlNavbar = () => {
-      if (window.scrollY > 700) {
-        setShow(false);
-      } else {
-        setShow(true);
-      }
-    };  
-  
-    useEffect(() => {
-      window.addEventListener('scroll', controlNavbar);
-      return () => {
-        window.removeEventListener('scroll', controlNavbar);
-      };
-    }, []);
-  
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  
-    const { pathname } = useLocation();
-    if (pathname !== '/')
-      return null;
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+    return () => {
+      window.removeEventListener('scroll', controlNavbar);
+    };
+  }, []);
+
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  const { pathname } = useLocation();
+  if (pathname !== '/')
+    return null;
+
+  const handleLogout = () => {
+    cookies.remove("Auth_TOKEN", { path: "/" });
+  };
 
   return (
     <div className={`nav ${show && 'nav_show'}`}>
-        <nav className='nav-body'>
-          <div className='mobile-nav'>
-            <Link to='home' spy={true} smooth={true} offset={0} duration={500} onClick={setClose}><div className='app-logo'><Logo/></div></Link>
-            <div className='hamburger' ><Hamburger color='black' direction='left' size={25} toggled={isOpen} toggle={setOpen}/></div>
-          </div>
-          <div className={`nav-area ${style_nav}`}>
-            <ul className='main'>
-                <li><Link to='about' activeClass="active" spy={true} smooth={true} offset={-165} duration={500} onClick={setClose}>About Us</Link></li>
-                <li><Link to='lessons' activeClass="active" spy={true} smooth={true} offset={-65} duration={500} onClick={setClose}>Lessons</Link></li>
-                <li><Link to='contact' activeClass="active" spy={true} smooth={true} offset={-50} duration={500} onClick={setClose}>Contact</Link></li>
-            </ul>
-            <ul className='sub'>
-                <li><a href='/login'>Login</a></li>
-                <li><a href='/register'>Register</a></li>
-                <li><img style={{width: '20px'}} src={user}/></li>
-                <li><a href='/register'>Logout</a></li>
-            </ul>   
-          </div>
-        </nav>
+      <nav className='nav-body'>
+        <div className='mobile-nav'>
+          <Link to='home' spy={true} smooth={true} offset={0} duration={500} onClick={setClose}><div className='app-logo'><Logo /></div></Link>
+          <div className='hamburger' ><Hamburger color='black' direction='left' size={25} toggled={isOpen} toggle={setOpen} /></div>
+        </div>
+        <div className={`nav-area ${style_nav}`}>
+          <ul className='main'>
+            <li><Link to='about' activeClass="active" spy={true} smooth={true} offset={-165} duration={500} onClick={setClose}>About Us</Link></li>
+            <li><Link to='lessons' activeClass="active" spy={true} smooth={true} offset={-65} duration={500} onClick={setClose}>Lessons</Link></li>
+            <li><Link to='contact' activeClass="active" spy={true} smooth={true} offset={-50} duration={500} onClick={setClose}>Contact</Link></li>
+          </ul>
+          <ul className='sub'>
+            {
+              cookies.get("Auth_TOKEN") ? (
+                <>
+                  <li><a href='/login'>Login</a></li>
+                  <li><a href='/register'>Register</a></li>
+                </>
+              ) : (
+                <>
+                  <li><img style={{ width: '20px' }} src={user} alt="User" /></li>
+                  <li><a href='/home' onClick={handleLogout}>Logout</a></li>
+                </>
+              )
+            }
+          </ul>
+        </div>
+      </nav>
     </div>
   )
 }
